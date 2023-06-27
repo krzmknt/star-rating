d44e2c3f7b9f79df2c2e206f5708385cfff24575
kurazumi@i-system.co.jp <kurazumi@i-system.co.jp>
2023-06-27 03:53:56 +0000

    [Update] スター数のクリック操作
    - レーティングの星の数を onClick で変更できるように修正

--- src/StarRating.tsx
+++ src/StarRating.tsx
@@ -6,12 +6,16 @@ const Star = ({ selected = false, onSelect = (f: unknown) => f }) => (
 );
 
 export const StarRating = ({ totalStars = 5 }) => {
-  const [selectedStars] = useState(5);
+  const [selectedStars, setSelectedStars] = useState(5);
 
   return (
     <>
       {[...Array(totalStars)].map((_, i) => (
-        <Star key={i} selected={i < selectedStars} onSelect={() => true} />
+        <Star
+          key={i}
+          selected={i < selectedStars}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
       ))}
       <p>
         {selectedStars} of {totalStars} stars
